f0aa9795bde6b3f0dfb9752cb66acaa9a2eec1bc
XIE Leon (BCI/ECM12-CN) <xel3szh@bosch.com>
2019-03-09 17:07:24 +0800

    first commit by Leon

---
+++ Exe_02.py
@@ -0,0 +1,5 @@
+# A commend, this is so you can read your problem laterself.
+
+print "I could have code like this." #and commend after is ignored;
+
+print "This will run."
---
+++ Exe_03.py
@@ -0,0 +1,13 @@
+#coding=utf-8  //测试
+
+print "I will now count my chichens:" #中文备注测试
+
+print "Hens",25 + 30 /6
+print "Roosters",100 - 25*3%4
+
+print 3+2+1 - 5 + 4%2 - 1.0/4 +6
+
+print "Is it true that 3+2 < 5-7?"
+print 3+2 < 5-7
+
+print "What is 3+2?",3+2 #����
